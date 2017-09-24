760b0ffc3a9a41241e466350736858f8b2afaaad
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-09-23 20:15:52 -0700

    - inital commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-        </p>
+      <div>
+        <h1>Hello World</h1>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
